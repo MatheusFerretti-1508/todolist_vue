7039fb0cbb1324636cdff6d2f54af94ac88f9eef
Matheus Ferretti <matheus10.ferretti@gmail.com>
2023-06-14 15:10:47 -0300

    Componentes 2

---
+++ src/components/Cabecalho.vue
@@ -0,0 +1,18 @@
+<script setup> 
+    const props = defineProps(['TarefasPendentes'])
+</script>
+
+<template>
+    <header class="p-5 mb-4 mt-4 bg-light rounded-3">
+        <h1>Minhas tarefas</h1>
+        <p>
+            Você possui {{ props.TarefasPendentes }} tarefas pendentes
+        </p>
+    </header>
+</template>
+
+<script>
+    export default {
+        name: "Cabecalho",
+    }
+</script>
---
+++ src/components/Formulario.vue
@@ -0,0 +1,19 @@
+<template>
+    <form @submit.prevent="cadastraTarefa">
+        <div class="row">
+            <div class="col">
+                <input :value="estado.tarefaTemp" @change="evento => estado.tarefaTemp = evento.target.value" required type="text" placeholder="Digite a descrição da tarefa" class="form-control">
+            </div>
+            <div class="col-md-2">
+                <button class="btn btn-primary" type="submit">Cadastrar</button>
+            </div>
+            <div class="col-md-2">
+                <select @change="evento => estado.filtro = evento.target.value" class="form-control">
+                    <option value="todas">Todas as tarefas</option>
+                    <option value="pendentes">Tarefas pendentes</option>
+                    <option value="finalizadas">Tarefas finalizadas</option>
+                </select>
+            </div>
+        </div>
+    </form>
+</template>
