9ff46b81fffca728b13fb52ce4e9b163d1855588
Matheus Ferretti <matheus10.ferretti@gmail.com>
2023-06-14 16:23:53 -0300

    Concluindo

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,23 @@
 <script setup>
   import { reactive } from 'vue';
-  import Cabecalho from  './components/Cabecalho.vue';
-  import Formulario from './components/Formulario.vue';
-  import ListaDeTarefas from './components/ListaDeTarefas.vue';
-
-
+  import Cabecalho from './components/Cabecalho.vue'
+  import Formulario from './components/Formulario.vue'
+  import ListaDeTarefas from './components/ListaDeTarefas.vue'
 
   const estado = reactive({
-    filtro: 'Todas',
+    filtro: 'todas',
     tarefaTemp: '',
     tarefas: [
       {
-        titulo: "Estudar ES6",
+        titulo: 'Estudar ES6',
         finalizada: false,
       },
       {
-        titulo: "Estudar SASS",
+        titulo: 'Estudar SASS',
         finalizada: false,
       },
       {
-        titulo: "Ir para academia",
+        titulo: 'Ir para a academia',
         finalizada: true,
       }
     ]
@@ -28,43 +26,38 @@
   const getTarefasPendentes = () => {
     return estado.tarefas.filter(tarefa => !tarefa.finalizada)
   }
+
   const getTarefasFinalizadas = () => {
     return estado.tarefas.filter(tarefa => tarefa.finalizada)
   }
 
   const getTarefasFiltradas = () => {
-    const {filtro} = estado;
+    const { filtro } = estado;
+
     switch (filtro) {
       case 'pendentes':
         return getTarefasPendentes();
       case 'finalizadas':
         return getTarefasFinalizadas();
-      default: 
+      default:
         return estado.tarefas;
-      }
+    }
   }
 
   const cadastraTarefa = () => {
     const tarefaNova = {
       titulo: estado.tarefaTemp,
-      finalizada: false
+      finalizada: false,
     }
-    estado.tarefas.push(tarefaNova)
-    estado.tarefaTemp = ''
+    estado.tarefas.push(tarefaNova);
+    estado.tarefaTemp = '';
   }
 </script>
 
 <template>
   <div class="container">
-    <Cabecalho />
-
-    <Formulario />
-    <ListaDeTarefas />
+    <Cabecalho :tarefas-pendentes="getTarefasPendentes().length" />
+    <Formulario :trocar-filtro="evento => estado.filtro = evento.target.value" :tarefa-temp="estado.tarefaTemp" :edita-tarefa-temp="evento => estado.tarefaTemp = evento.target.value" :cadastra-tarefa="cadastraTarefa" />
+    <ListaDeTarefas :tarefas="getTarefasFiltradas()" />
   </div>
 </template>
-
-<style scoped>
-  .done {
-    text-decoration: line-through;
-  }
-</style>
--- src/components/Cabecalho.vue
+++ src/components/Cabecalho.vue
@@ -1,18 +1,11 @@
-<script setup> 
-    const props = defineProps(['TarefasPendentes'])
+<script setup>
+    const props = defineProps(['tarefasPendentes'])
 </script>
-
 <template>
     <header class="p-5 mb-4 mt-4 bg-light rounded-3">
         <h1>Minhas tarefas</h1>
         <p>
-            Você possui {{ props.TarefasPendentes }} tarefas pendentes
+            Você possui {{ props.tarefasPendentes }} tarefas pendentes
         </p>
     </header>
 </template>
-
-<script>
-    export default {
-        name: "Cabecalho",
-    }
-</script>
